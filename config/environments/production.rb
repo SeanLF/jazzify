20f8689d1f72c7ae339b7bb65f6bdbf1521de6d4
Sean Floyd <sfloy029@uottawa.ca>
2015-01-22 00:36:17 -0500

    Change config file
    Fix production config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Testapp::Application.configure do
+Jazzify::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
