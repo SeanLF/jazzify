131dc4456ebdc9b389cc55702201c1cf2cb230fc
Sean Floyd <sfloy029@uottawa.ca>
2015-12-18 21:47:08 -0500

    Update gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Jazzify::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'ottawajazzify.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        "smtp.mandrillapp.com",
